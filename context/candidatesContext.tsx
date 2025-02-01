9a78df509eed73aae9904d53e7db5d3b783d3bdc
Alanmad06 <madrigal.saenz.alan@gmail.com>
2025-02-01 11:19:16 -0600

    Fetch candidates from API and update state in CandidatesProvider; add error handling for data retrieval

--- context/candidatesContext.tsx
+++ context/candidatesContext.tsx
@@ -1,15 +1,43 @@
 'use client'
 
-import {  actionCandidate, CandidatesContextProps, selectedCandidate } from '@/interfaces/candidates';
-import React, { createContext, useContext, ReactNode, useReducer } from 'react';
+import { actionCandidate, CandidatesContextProps, selectedCandidate } from '@/interfaces/candidates';
+import React, { createContext, useContext, ReactNode, useReducer, useEffect } from 'react';
 import reducer from '@/lib/reducer/candidatesReducer';
 
 const CandidatesContext = createContext<CandidatesContextProps | undefined>(undefined);
 
 export const CandidatesProvider = ({ children }: { children: ReactNode }) => {
-    /* localStorage.getItem('candidates') ? JSON.parse(localStorage.getItem('candidates') as string)  */
-    const [ candidates, dispatch ] = useReducer<selectedCandidate[], [action: actionCandidate]>(reducer,[]);
-    console.log('context',candidates)
+
+    // Estado inicial vacío hasta que se carguen los datos
+    const initialState: selectedCandidate[] = [];
+
+    const [candidates, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+        const fetchCandidates = async () => {
+            try {
+                const response = await fetch('/api/getvotes'); 
+                const data = await response.json();
+                
+                console.log('Data from API:', data);
+
+                // Convertir los datos en un array si es necesario
+                const votesArray = Object.keys(data.result.votes).map(key => ({
+                    ...data.result.votes[key]
+                }));
+
+                console.log('Processed votes:', votesArray);
+
+                // Enviar los datos a useReducer
+                dispatch({ type: 'SET_CANDIDATES', payload: votesArray });
+            } catch (error) {
+                console.error('Failed to fetch candidates', error);
+            }
+        };
+
+        fetchCandidates();
+    }, []);
+
     return (
         <CandidatesContext.Provider value={{ candidates, dispatch }}>
             {children}
@@ -19,7 +47,8 @@ export const CandidatesProvider = ({ children }: { children: ReactNode }) => {
 
 export const useCandidates = (): CandidatesContextProps => {
     const context = useContext(CandidatesContext);
-    
+    console.log('context', context);
+
     if (!context) {
         throw new Error('useCandidates must be used within a CandidatesProvider');
     }
