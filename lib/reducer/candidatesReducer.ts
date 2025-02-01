51560b7f9360a743646afdb42bc4c3341926b88b
Alanmad06 <madrigal.saenz.alan@gmail.com>
2025-02-01 11:18:55 -0600

    Enhance candidates reducer to handle default state and add SET_CANDIDATES action

--- lib/reducer/candidatesReducer.ts
+++ lib/reducer/candidatesReducer.ts
@@ -1,13 +1,13 @@
 import { selectedCandidate, actionCandidate } from "@/interfaces/candidates";
 
-const reducerCandidates = (state: selectedCandidate[], action: actionCandidate) :selectedCandidate[] => {
+const reducerCandidates = (state: selectedCandidate[] = [], action: actionCandidate) :selectedCandidate[] => {
 
     
     switch(action.type){
         case 'ADD_CANDIDATE':
             /* localStorage.setItem('candidates', JSON.stringify([...state, action.payload])); */
             if (action.payload) {
-                return [...state, action.payload];
+                return [...state, action.payload as selectedCandidate];
             }
             return state;
         case 'REMOVE_CANDIDATE':
@@ -15,13 +15,19 @@ const reducerCandidates = (state: selectedCandidate[], action: actionCandidate) 
             return [...state]
         case 'UPDATE_CANDIDATE':
             if(action.payload){
-                const index = state.findIndex(candidate => candidate.category=== action.payload?.category)
+                
+                const index = state.findIndex((candidate: selectedCandidate) => candidate.category === (action.payload as selectedCandidate).category)
                 if(index!==-1){
-                    state[index] = action.payload
+                    state[index] = action.payload as selectedCandidate;
                     return [...state]
                 }
             
             }
+        case 'SET_CANDIDATES':
+            if(action.payload){
+                return [...state,...action.payload as selectedCandidate[]]
+            }
+
         default:
             return state;
     }
