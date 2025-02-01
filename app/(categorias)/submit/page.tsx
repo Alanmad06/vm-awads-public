5f1ae42b29be301f4690a9145a79d29660c6aed3
Alanmad06 <madrigal.saenz.alan@gmail.com>
2025-02-01 16:03:31 -0600

    Wrap SubmitButton in a styled container for improved layout

--- app/(categorias)/submit/page.tsx
+++ app/(categorias)/submit/page.tsx
@@ -2,10 +2,14 @@
 
 import SubmitButton from "@/components/submitButton";
 import { useCandidates } from "@/context/candidatesContext";
+import styles from './page.module.css'
 
 export default function SubmitPage(){
     const {candidates} = useCandidates()
     return(
-        <SubmitButton selectedCandidates={candidates} />
+        <div className={styles.container}>
+            <SubmitButton selectedCandidates={candidates} />
+        
+        </div>
     )
 }
