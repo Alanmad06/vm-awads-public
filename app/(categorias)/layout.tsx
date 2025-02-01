3578661c7c4f9cbfefc1e2803b007c1e997f8643
Alanmad06 <madrigal.saenz.alan@gmail.com>
2025-02-01 11:17:47 -0600

    Add CandidatesProvider to layout and create SubmitPage component with SubmitButton

--- app/(categorias)/layout.tsx
+++ app/(categorias)/layout.tsx
@@ -3,16 +3,17 @@
 import SignOutButton from "@/components/signOutButton";
 import NavigationButton from "@/components/navigationButton";
 import styles from "./layout.module.css"
+import { CandidatesProvider } from "@/context/candidatesContext";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 
   return (
-    <>
+    <CandidatesProvider>
     <div className={styles.nav}>
       <NavigationButton/>
       <SignOutButton/>
       </div>
       {children}
-    </>
+    </CandidatesProvider>
   );
 }
