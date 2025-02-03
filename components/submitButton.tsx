25000d9b4c2db42271b0d1025337f15f617f0e30
Alanmad06 <madrigal.saenz.alan@gmail.com>
2025-02-03 16:20:23 -0600

    Fixed submitButton disabled logic changed disabled state for a variable And Change submit page toast style

--- components/submitButton.tsx
+++ components/submitButton.tsx
@@ -1,12 +1,27 @@
-
+"use client"
 import { selectedCandidate } from "@/interfaces/candidates"
+import { toast } from "react-toastify";
 import styles from "./styles/submitButton.module.css"
+import { useState } from "react";
 
 export default function submitButton({ selectedCandidates, index }: { selectedCandidates: selectedCandidate[],  index: number }) {
 
+    let disabled = true
+    
     if(selectedCandidates.length > 15) selectedCandidates.pop()
+        if(selectedCandidates.length === 15) disabled = false
     
     const handleVote = async () => {
+        
+        if(disabled) {
+           toast.info('Debes votar en todas las categorias',{
+            className: styles.toast,
+            closeOnClick: true
+            
+           })
+           return
+        }
+        
         try {
             const response = await fetch('api/vote', {
                 method: 'POST',
@@ -28,7 +43,7 @@ export default function submitButton({ selectedCandidates, index }: { selectedCa
         <div className={`${styles.stars} ${styles.stars1}`} ></div>
         <div className={`${styles.stars} ${styles.stars2}`} ></div>
         <div className={`${styles.stars} ${styles.stars3}`} ></div>
-        <button className={styles.form__button} onClick={handleVote} type="submit">
+        <button aria-disabled={disabled} className={styles.form__button} onClick={handleVote} type="submit">
             Enviar <br /> votos
         </button>
    </div>
