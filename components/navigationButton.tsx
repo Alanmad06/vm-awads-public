4bc68dd3dbfff4cce6a832267cce0f6cedf6393b
Alanmad06 <madrigal.saenz.alan@gmail.com>
2025-02-03 16:18:53 -0600

    Update navigation node logic and Show categories in button

--- components/navigationButton.tsx
+++ components/navigationButton.tsx
@@ -4,12 +4,15 @@ import navigationList from "@/lib/navigation";
 import { usePathname, useRouter } from "next/navigation";
 import styles from './styles/navigationButton.module.css'
 import { ArrowLeft, ArrowRight } from "lucide-react";
+import { Node } from "dbly-linked-list";
 
 export default function NavigationButton({forward}:{forward : boolean}) {
 
 
     const router = useRouter();
     const pathname = usePathname();
+    const node : Node | number = navigationList.find(pathname!);
+    
     return (
 
     
@@ -17,20 +20,18 @@ export default function NavigationButton({forward}:{forward : boolean}) {
             <div className={`${styles.stars} ${styles.stars1}`} ></div>
             <div className={`${styles.stars} ${styles.stars2}`} ></div>
             <div className={`${styles.stars} ${styles.stars3}`} ></div>
-            {(forward) ? <button className={`${styles.form__button} ${styles.forward}`} onClick={() => {
-                const node = navigationList.find(pathname!);
-                if (node instanceof Object) {
-                    router.push(node.next?.data.toString() ?? '/');
-                }
-            }}><ArrowRight size={15}/>
+            {(forward) ?  <button className={`${styles.form__button} ${styles.forward}`} onClick={() => {
+                
+                 router.push((node as Node).next?.data.toString() ?? '/');
+                   
+                
+            }}>{(node as Node).next?.data.toString().slice(1)  ?? 'Inicio' }{" "}<ArrowRight size={15}/>
             </button>
             :
             <button className={styles.form__button} onClick={() => {
-                const node = navigationList.find(pathname!);
-                if (node instanceof Object) {
-                    router.push(node.prev?.data.toString() ?? '/');
-                }
-            }}><ArrowLeft size={15}/> {" "}Categoria 
+                
+                 router.push((node as Node).prev?.data.toString() ?? '/');
+            }}><ArrowLeft size={15}/> {" "}{(node as Node).prev?.data.toString().slice(1)  ?? 'Incio' }
                 
             </button>
 }
