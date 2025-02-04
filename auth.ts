3f8dcd8d055e99ca0f3dac135f50810142b51728
Alanmad06 <madrigal.saenz.alan@gmail.com>
2025-02-04 11:56:05 -0600

    Refactor authentication logic; improve error handling and add nodemailer dependency

--- auth.ts
+++ auth.ts
@@ -1,49 +1,42 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { z } from "zod";
 import bcrypt from "bcryptjs";
-
 import { authConfig } from "./auth.config";
 import { getUser } from "./lib/db/getUser";
-import { getVotes } from "./lib/db/getVotes";
 import { loginSchema } from "./lib/schemas/authSchemas";
-
-
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
   providers: [
     CredentialsProvider({
       async authorize(credentials) {
-        const parsedCrendials = loginSchema.safeParse(credentials)
+        const parsedCrendials = loginSchema.safeParse(credentials);
 
-          if(!parsedCrendials.success){
-            return null
-          }
+        if (!parsedCrendials.success) {
+          return null;
+        }
 
-          const {email , password} = parsedCrendials.data;
-          try{
+        const { email, password } = parsedCrendials.data;
+        try {
           const user = await getUser(email);
 
-          if(!user) {
+          if (!user) {
             console.error("Usuario no encontrado");
             return null;
           }
 
-          const votes = await getVotes(user.id)
-          console.log('votes' , votes)
-         
-          const isPasswordValid = await bcrypt.compare(password, user.password!);
-         
+          const isPasswordValid = await bcrypt.compare(
+            password,
+            user.password!
+          );
+
           if (!isPasswordValid) {
             console.error("Contraseña incorrecta");
             return null;
           }
 
-          
-          return {id: user.id, email: user.email,name :user.name};
-  
-        }catch (error) {
+          return { id: user.id, email: user.email, name: user.name };
+        } catch (error) {
           console.error("Error durante la autorización:", error);
           return null;
         }
@@ -53,16 +46,14 @@ export const { auth, signIn, signOut } = NextAuth({
   callbacks: {
     jwt({ token, user }) {
       if (user) {
-        token.id = user.id; 
+        token.id = user.id;
       }
       return token;
     },
-     session({ session, token }) {
-      
+    session({ session, token }) {
       session.user.id = token.id as string;
-      
+
       return session;
     },
-  }
-
+  },
 });
