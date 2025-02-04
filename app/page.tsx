e7095af06abe9f96a611381888339c9335761725
Alanmad06 <madrigal.saenz.alan@gmail.com>
2025-02-03 22:24:02 -0600

    Update layout styles for improved spacing and add SignOutButton component

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import HomeSession from "@/components/homeSession";
 import styles from "./page.module.css"
 import HomeVideo from "@/components/homeVideo";
 import HomeTrailer from "@/components/homeTrailer";
+import SignOutButton from "@/components/signOutButton";
 
 export default function Home() {
 
   return (
     <div className={styles.container}>
+     
       <HomeSession/>
       <HomeVideo/>
       <HomeTrailer/>
