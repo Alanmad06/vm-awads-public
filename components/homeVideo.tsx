6b80122bb590be104f204df56826222bde5ae490
Alanmad06 <madrigal.saenz.alan@gmail.com>
2025-02-03 22:24:31 -0600

    Refactor HomeVideo and VmAnimation components; adjust z-index in CSS and update video source type

--- components/homeVideo.tsx
+++ components/homeVideo.tsx
@@ -1,8 +1,9 @@
+"use client"
 import styles from './styles/homeVideo.module.css'
 
 export default function HomeVideo() {
     return (
-        <div className={styles.container}>
+        <div className={styles.container} >
             <video className={styles.video} controls={false} autoPlay muted playsInline loop>
                 <source src="/videos/vm_wallpaper.webm" type="video/webm" />
                 Tu navegador no soporta el formato de video.
--- components/vmAnimation.tsx
+++ components/vmAnimation.tsx
@@ -4,7 +4,7 @@ export default function VmAnimation() {
     return (
         <div className={styles.container}>
             <video controls={false} autoPlay muted playsInline loop>
-        <source src="/videos/vm_animation.webm"  type="video/webm" />
+        <source src="/videos/vm_animation.webm"  type="video/mp4" />
         Tu navegador no soporta el formato de video.
       </video>
         </div>
