e1e351e558ae01b3a10ab01fcf2b84540f93d6e3
Alanmad06 <madrigal.saenz.alan@gmail.com>
2025-02-05 19:34:27 -0600

    Implement Google sign-in user creation and enhance authorization flow

--- auth.ts
+++ auth.ts
@@ -5,16 +5,18 @@ import { authConfig } from "./auth.config";
 import { getUser } from "./lib/db/getUser";
 import { loginSchema } from "./lib/schemas/authSchemas";
 import Google from "next-auth/providers/google";
+import { Pool } from "pg";
+import { createUser, createUserWIithID } from "./lib/db/createUser";
+
+
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
+  
   providers: [
     CredentialsProvider({
       async authorize(credentials) {
-        
         const parsedCrendials = loginSchema.safeParse(credentials);
-
-       
         if (!parsedCrendials.success) {
           return null;
         }
@@ -22,24 +24,16 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         const { email, password } = parsedCrendials.data;
         try {
           const user = await getUser(email);
-
-        
           if (!user) {
             console.error("Usuario no encontrado");
             return null;
           }
 
-          const isPasswordValid = await bcrypt.compare(
-            password,
-            user.password!
-          );
-
+          const isPasswordValid = await bcrypt.compare(password, user.password!);
           if (!isPasswordValid) {
             console.error("Contraseña incorrecta");
             return null;
           }
-
-          
 
           return { id: user.id, email: user.email, name: user.name };
         } catch (error) {
@@ -53,23 +47,33 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  pages :{
-    error : '/error'
+  pages: {
+    error: "/error",
   },
   callbacks: {
-    jwt({ token, user }) {
+    async signIn({ user, account }) {
+      if (account?.provider === "google") {
+        try {
+          const isUser = !!await getUser(user.email!);
+          if(!isUser)  await createUserWIithID(user.id!,user.email!, user.name!, "")
+       
+        
+        } catch (error) {
+          console.error("Error guardando usuario de Google:", error);
+          return false; // Evita que el usuario inicie sesión si hay un error
+        }
+      }
+      return true;
+    },
+    async jwt({ token, user }) {
       if (user) {
         token.id = user.id;
       }
       return token;
     },
-    session({ session, token }) {
+    async session({ session, token }) {
       session.user.id = token.id as string;
-
       return session;
     },
   },
-
 });
-
-
