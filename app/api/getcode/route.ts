0866be588e5a7fa78c144420bc0d4c6ba59e1471
Alanmad06 <madrigal.saenz.alan@gmail.com>
2025-02-05 20:07:34 -0600

    Refactor POST request handler in getcode route to improve error logging and remove unused imports

--- app/api/getcode/route.ts
+++ app/api/getcode/route.ts
@@ -1,19 +1,17 @@
 import { createCode } from "@/lib/db/verification_codes/createCode";
-import { sendVerificationEmail } from "@/lib/email";
+
 
 export async function POST(request : Request) {
   try {
     const { email } = await request.json();
     if (!email) return new Response("Email es requestuerido", { status: 400 });
 
-    
-    
-
     await createCode(email)
    
 
     return new Response("Código enviado", { status: 200 });
   } catch (error) {
+    console.error(error)
     return new Response("Error enviando el email", { status: 500 });
   }
 }
