086aa53ced4c2736bdd8f4494b1ac77ac0d05a9e
Alanmad06 <madrigal.saenz.alan@gmail.com>
2025-02-05 19:35:20 -0600

    Refactor user creation and voting retrieval to use email instead of ID; add password handling in createUser function

--- lib/db/createUser.ts
+++ lib/db/createUser.ts
@@ -4,12 +4,45 @@ import { pool } from "./db"
 import { User } from "@/interfaces/candidates"
 import { hash } from "bcryptjs"
 
-export async function createUser(email:string , name : string , password : string) : Promise<User | undefined>{
+export async function createUser(email:string , name : string , password : string | undefined ) : Promise<User | undefined>{
     const db = await pool.connect()
     try{
-       const hashPassword = await hash(password,10)
-       const result : QueryResult<any> = await db.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3)',[name,email,hashPassword])
-      return(result.rows[0])
+       
+        if(password){
+            const hashPassword = await hash(password,10)
+           const result : QueryResult<any> = await db.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3)',[name,email,hashPassword])
+            return(result.rows[0])
+        }else{
+            const result : QueryResult<any> = await db.query('INSERT INTO users (name, email) VALUES ($1, $2)',[name,email])
+             return(result.rows[0])
+        }
+       
+      
+  
+    }catch(e){
+     console.error(e)
+     return 
+    }finally{
+      db.release();
+    }
+  
+  }
+
+  export async function createUserWIithID(id:string,email:string , name : string , password : string | undefined ) : Promise<User | undefined>{
+    const db = await pool.connect()
+    try{
+       
+        console.log("IDDDDD",id)
+        if(password){
+            const hashPassword = await hash(password,10)
+           const result : QueryResult<any> = await db.query('INSERT INTO users (id,name, email, password) VALUES ($1, $2, $3,$4)',[id,name,email,hashPassword])
+            return(result.rows[0])
+        }else{
+            const result : QueryResult<any> = await db.query('INSERT INTO users (id,name, email) VALUES ($1, $2,$3)',[id,name,email])
+             return(result.rows[0])
+        }
+       
+      
   
     }catch(e){
      console.error(e)
--- lib/db/getVotes.ts
+++ lib/db/getVotes.ts
@@ -5,11 +5,12 @@ type Votes ={
   votes : {}
 }
 
-export async function getVotes(id:string) : Promise<Votes | undefined>{
+export async function getVotes(email:string) : Promise<Votes | undefined>{
     const db = await pool.connect()
+   
     try{
         
-      const result : QueryResult<Votes> = await db.query('SELECT votes FROM users_votes WHERE user_id = ($1)',[id])
+      const result : QueryResult<Votes> = await db.query('SELECT votes FROM users_votes WHERE email = ($1)',[email])
     
       return(result.rows[0])
   
