89f61dab0b5b31a7ad0a3ceaacf8dd1e7771bea8
Alanmad06 <madrigal.saenz.alan@gmail.com>
2025-02-10 14:30:26 -0600

    Update results page

--- app/resultados/page.tsx
+++ app/resultados/page.tsx
@@ -4,7 +4,7 @@ import Results from "@/components/results";
 
 export default function StatsChart() {
 
-
+//ola
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>RESULTADOS</h1>
